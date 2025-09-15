98797a3934ae4a7a3bde75da246b7c7c8e203bd3
IT24103987 <it24103987@my.sliit.lk>
2025-09-15 23:09:07 +0530

    IT24103987_Lab06

---
+++ Lab Sheet 06/IT24103987.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\fathi\\OneDrive\\Desktop\\IT24103987")
+getwd()
+
+#Distribution of X
+0.85
+
+#Probability that at least 47 students passed the test
+dbinom(47,50,0.85)
+
+#Random variable (X)
+Number of calls received in one hour
+
+#Distribution of X
+lamda = 12
+
+#Probability that exactly 15 calls are received in an hour
+dpois(15,12)
